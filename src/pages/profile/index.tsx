0692f1a8ecc973a754bc306bc9e0bd56d62da973
arthur.barre <arthur.barre@easy.movie>
2021-07-22 17:49:37 +0200

    add forgot password and profile pages

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,7 +1,8 @@
 import classNames from 'classnames/bind';
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '../../components/atoms/Button/Button';
+import Label from '../../components/atoms/Label/Label';
 import styles from './Profile.module.scss';
-
 const c = classNames.bind(styles);
 
 interface ProfileProps {
@@ -9,9 +10,48 @@ interface ProfileProps {
 }
 
 function Profile({ className }: ProfileProps) {
+	const [isChangingPassword, setIsChangingPassword] = useState(false);
 	return (
 		<div className={c('wrapper', className)}>
-			<p>Profile</p>
+			{isChangingPassword ? (
+				<>
+					<h2 className={c('title')}>
+						Vous souhaitez changer de mot de passe ?
+					</h2>
+					<p className={c('description')}>
+						Vous recevrez dans votre boite mail un lien afin de réinitialiser
+						votre mot de passe.
+					</p>
+					<button
+						onClick={() => setIsChangingPassword(!isChangingPassword)}
+						className={c('link')}
+					>
+						Retour au profil
+					</button>
+					<Button onClick={() => {}} className={c('button')}>
+						Envoyer le lien
+					</Button>
+				</>
+			) : (
+				<>
+					<h2 className={c('title')}>Votre profil </h2>
+					<p className={c('description')}>
+						Vous recevrez dans votre boite mail un lien afin de réinitialiser
+						votre mot de passe.
+					</p>
+					<Label className={c('label-wrapper')}>Mail</Label>
+					<div className={c('description')}>johndoe@monecole.fr</div>
+					<button
+						onClick={() => setIsChangingPassword(!isChangingPassword)}
+						className={c('link')}
+					>
+						Modifier le mot de passe ?
+					</button>
+					<Button onClick={() => {}} className={c('button')}>
+						Se déconnecter
+					</Button>
+				</>
+			)}
 		</div>
 	);
 }
